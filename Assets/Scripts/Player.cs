8caad8977c8533d17ec15cadf4cfeeeaa420e0e9
maringernina <nina.maringer01@htl-salzburg.ac.at>
2021-03-21 23:51:36 +0100

    add Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 targetPos;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+       if (Input.GetMouseButtonDown(0))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * 5);
+
+       
     }
 }
