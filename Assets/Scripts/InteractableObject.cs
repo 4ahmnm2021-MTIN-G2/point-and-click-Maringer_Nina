22dfd04805d6a10f973aaa3a703bf956056cf78f
maringernina <nina.maringer01@htl-salzburg.ac.at>
2021-04-16 00:46:30 +0200

    add object sprites and interactable object script

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    public GameObject Object;
+    public Text ObjectText;
+    public string TextValue;
+    
+
+    void Start ()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Object.SetActive(false);
+        ObjectText.text = TextValue;
+    }
+
+
+}
