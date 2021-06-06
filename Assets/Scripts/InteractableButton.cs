844ee1af85ee286ca9f4d6c24e43e816d6db2769
maringernina <nina.maringer01@htl-salzburg.ac.at>
2021-06-06 22:51:38 +0200

    add updated Assets

---
+++ Assets/Scripts/InteractableButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class InteractableButton : MonoBehaviour
+{
+    public Text ObjectText;
+    public UnityEvent clicked;
+    public Transform currentPosition;
+    public Transform btnPosition;
+
+
+    private void OnMouseDown()
+    {
+        ObjectText.text = this.gameObject.name;
+        //this.gameObject.SetActive(false);
+
+        clicked.Invoke();
+
+        //btnPosition.position = currentPosition.position;
+    }
+
+
+
+
+}
--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System.Collections;
 
 public class InteractableObject : MonoBehaviour
 {
-
     public Text ObjectText;
     public UnityEvent clicked;
 
     private void OnMouseDown()
     {
         ObjectText.text = this.gameObject.name;
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(true);
+        ObjectText.GetComponent<Text>().enabled = true;
+       // GetComponent<SpriteRenderer>().enabled = false;
 
         clicked.Invoke();
+
+        StartCoroutine(Deactivate());
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(3);
+        ObjectText.GetComponent<Text>().enabled = false;
     }
 
 
